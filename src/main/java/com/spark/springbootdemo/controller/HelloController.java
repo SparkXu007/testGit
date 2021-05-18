4a80c44a1690d3ceea870c4590dfd3047eff70d0
xukang <1147971848@qq.com>
2021-05-19 06:12:35 +0800

    添加文件

--- src/main/java/com/spark/springbootdemo/controller/HelloController.java
+++ src/main/java/com/spark/springbootdemo/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     @RequestMapping("/hello")
 //    @ResponseBody
     public String hello(){
-        return "hello, spark.";
+        return "hello, spark2.";
     }
 }
